03ed47830347f10ed6de246a13a51d11ee6561c1
HenriqueMAlves <m.henriquealves@gmail.com>
2022-11-22 06:56:02 -0300

    perf: adicionar validação para a operação de divisão

--- Seminario/calculator.js
+++ Seminario/calculator.js
@@ -1,8 +1,25 @@
-const calculator = {
-    sum:  (a, b) => a + b,
-    diff: (a, b) => a - b,
-    mult: (a, b) => a * b,
-    div:  (a, b) => a / b,
+class calculator{
+    sum(a, b) {
+        return a + b;
+    }
+
+    diff(a, b) {
+        return a - b;
+    }
+    
+    mult(a, b) {
+        return a * b;
+    }
+    
+    div(a, b) {
+        if(b != 0){
+            return a / b;
+        } 
+        else{
+            return "invalid operation";
+        }
+
+    }
 }
 
 module.exports = calculator;
